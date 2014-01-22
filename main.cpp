d1593c88ad078a0e962f789f985829f6d9db18d3
Wojciech Żmuda <zmuda.w@gmail.com>
2014-01-22 17:09:05 +0100

    main(): generator, wypisz, sum, min, max

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+#include <iostream>
+#include "utils.h"
+
 int main() {
+    int tablica[] = generator();
+    wypisz(tablica);
+    cout << "Suma tablicy: " << suma(tablica) << std::endl;
+    cout << "Min tablicy: " << min(tablica) << std::endl;
+    cout << "Max tablicy: " << max(tablica) << std::endl;
     return 0;
 }
