0b93f8330eb44b60ce8d7d1cf87c210c9a1dceb7
kiperek <monikacabaj@vp.pl>
2014-01-22 17:10:34 +0100

    merged .h files

--- utils.h
+++ utils.h
@@ -1,3 +1,15 @@
+int generator()
+{
+	int t[10];
+	srand ( time(NULL) );
+
+	for(int i=0;i<10;i++)
+	{
+		t[i]=rand() % 101;
+	}
+	return t
+}
+
 void wypisz(int tab[])
 {
 	for(int i=0;i<10;i++)
@@ -14,4 +26,30 @@ int suma(int tab[])
 		res += tab[i];
 	}
 	return res;
+}
+
+int min(int array)
+{
+     int length = array.size( );  // establish size of array
+     int min = array[0];       // start with min = first element
+
+     for(int i = 1; i<length; i++)
+     {
+          if(array[i] < min)
+                min = array[i];
+     }
+     return min;                // return min value in array
+}
+
+int max(int array)
+{
+     int length = array.size( );  // establish size of array
+     int max = array[0];       // start with max = first element
+
+     for(int i = 1; i<length; i++)
+     {
+          if(array[i] > max)
+                max = array[i];
+     }
+     return max;                // return max value in array
 }
